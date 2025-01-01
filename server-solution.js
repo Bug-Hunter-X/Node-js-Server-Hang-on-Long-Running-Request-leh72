9549e83841e4c587376a0d53dfd31e7e666183be
BugHunter-X <askzenai@gmail.com>
2025-01-01 10:42:07 +0100

    Add server-solution.js file

---
+++ server-solution.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const { Worker } = require('worker_threads');
+
+const server = http.createServer((req, res) => {
+  const worker = new Worker('./long-task.js');
+  worker.on('message', (result) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Hello World! Result: ${result}`);
+  });
+  worker.on('error', (err) => {
+    console.error(err);
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Internal Server Error');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+// long-task.js
+const { parentPort } = require('worker_threads');
+
+const start = Date.now();
+while (Date.now() - start < 5000) {
+  // Simulate long task
+}
+parentPort.postMessage('Long task completed!');
